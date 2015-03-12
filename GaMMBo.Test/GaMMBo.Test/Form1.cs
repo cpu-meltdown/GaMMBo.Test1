dee11cd4e5a4439cf09b5ee5f4d89381be7bddfa
cpu-meltdown <n.baalbaki91@gmail.com>
2015-03-12 03:13:57 -0400

    modified the form

--- GaMMBo.Test/GaMMBo.Test/Form1.cs
+++ GaMMBo.Test/GaMMBo.Test/Form1.cs
@@ -20,22 +20,32 @@ namespace GaMMBo.Test
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            SqlConnection conn = new SqlConnection(Properties.Settings.Default.CategoriesConnectionString);
+            // TODO: This line of code loads data into the 'categoriesDataSet1.Music' table. You can move, or remove it, as needed.
+            this.musicTableAdapter.Fill(this.categoriesDataSet1.Music);
+            //SqlConnection conn = new SqlConnection(Properties.Settings.Default.CategoriesConnectionString);
 
-            SqlCommand selectCommand = new SqlCommand("select Artist, Album, Year from Music Order By Year", conn);
+            //SqlCommand selectCommand = new SqlCommand("select Artist, Album, Year from Music Order By Year", conn);
 
-            conn.Open();
-            SqlDataReader reader = selectCommand.ExecuteReader();
+            //conn.Open();
+            //SqlDataReader reader = selectCommand.ExecuteReader();
 
-            string selectedMovies = string.Empty;
+            //string selectedMovies = string.Empty;
 
-            while (reader.Read() == true)
-            {
-                selectedMovies += "Artist: " + reader[0] + " Album: " + reader[1] + " Year: "+ reader[2] +"\n";
-            }
+            //while (reader.Read() == true)
+            //{
+            //    selectedMovies += "Artist: " + reader[0] + " Album: " + reader[1] + " Year: "+ reader[2] +"\n";
+            //}
             
-            MessageBox.Show(selectedMovies);
-            conn.Close();
+            //MessageBox.Show(selectedMovies);
+            //conn.Close();
+        }
+
+        private void musicBindingNavigatorSaveItem_Click(object sender, EventArgs e)
+        {
+            this.Validate();
+            this.musicBindingSource.EndEdit();
+            this.tableAdapterManager.UpdateAll(this.categoriesDataSet1);
+
         }
     }
 }
