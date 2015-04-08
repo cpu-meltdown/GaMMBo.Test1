aa14db2a467fe15c4aa18a7bc1acc8ffc0dc2456
yassine93 <bassima@cox.net>
2015-04-07 22:24:47 -0400

    added comments

--- GaMMBo.Test/GaMMBo.Test/PrefMenu.cs
+++ GaMMBo.Test/GaMMBo.Test/PrefMenu.cs
@@ -12,16 +12,18 @@ using System.Data.SqlClient;
 
 namespace GaMMBo.Test
 {
-
+    //if you guys need me to explain anything just let me know 
     public partial class PrefMenu : Form
     {
         public int skipNum = 0;// variable that keeps track how many times the user skips
         public int choice;// variable keeps track of what category is being accessed
         public int objectId;//the id of the object being voted on
-        public int userId = 4;//if the voting being done is by a user their id is stored in this variable
-        public int numOfvotes = 0;// keeps track how many objects having been voted on 
+        public int userId = 4;//if the voting being done is by a user their id is stored in this variable its set to 4 only so i could test 
+                               // when the user logs in the number will be sent here like the choice 
+        public int numOfvotes = 0;// keeps track how many objects having been voted on hasnt be used yet will be
         public int vote = 0; // variable for the userlinker tables 
-        public Boolean userVoting = false;
+        public Boolean userVoting = false;//will be set prior to this point when the user either logs in or clicks guest
+        // the rest are self explanatory i may move or remove at the end before i transfer all to proxy
         Random randomNumber = new Random();
         String type = null;
         Image image = null;
@@ -34,7 +36,9 @@ namespace GaMMBo.Test
             //choice = c;
         }
 
-
+        // whats left to be done which i will do is create a new table to hold guests like so results can be given to them at the end 
+        // i will also add a variable to keep track of how many objects they have voted on and cut it off at 10
+        // also is left is updating the userbooks, usermovies, etc table to increment when they like an object
         private void button1_Click(object sender, EventArgs e)
         {
             // like button
@@ -58,7 +62,7 @@ namespace GaMMBo.Test
             // disklike button 
             if (userVoting)
             {
-                this.likeOrdislikeObject(1);
+                this.likeOrdislikeObject(0);
                 this.getUserObject();
             }
             else
@@ -70,7 +74,7 @@ namespace GaMMBo.Test
 
         private void button3_Click(object sender, EventArgs e)
         {
-            // skip button
+            // skip button limit is set to 5 skips if the user reaches five a message pops up telling them to either like or dislike
             if (skipNum < 5)
             {
                 if (userVoting)
@@ -116,7 +120,7 @@ namespace GaMMBo.Test
 
         private void PrefMenu_Load(object sender, EventArgs e)
         {
-            if (userVoting)
+            if (userVoting)//userVoting is a boolean that will be set when the user either logs in or will enter as a guest
             {
                 this.getUserObject();
             }
@@ -129,7 +133,7 @@ namespace GaMMBo.Test
         }
         private void getGuestObject()
         {
-
+            // goes into database and prints random object 
 
             if (choice == 1)//generates music 
             {
@@ -178,7 +182,9 @@ namespace GaMMBo.Test
         }
 
         public void likeOrdislikeObject(int v)
-        {
+        {   
+            // this method inserts a voted object into the linker tables the parameter v can either be 0 for dislike or 1 for like
+            
             vote = v;
 
 
@@ -213,8 +219,8 @@ namespace GaMMBo.Test
         }
 
 
-        private void getUserObject()
-        {
+        private void getUserObject()// takes into account objects that have already been voted on
+        {    
             String name = null;
 
             String linker = null;
@@ -252,7 +258,7 @@ namespace GaMMBo.Test
                 linker = "UserGameLinker";
                 objectId = randomNumber.Next(84);
                 image = Image.FromFile(@"C:\GaMMBo.Test1\Games_Images\" + objectId + ".jpg");
-            }
+            }//this sql command checks if that particular object and user is in the specific linker table 
             sqlCommand =
                                        new SqlCommand("Select " + type + ".Name  From " + type +
                                         " Inner Join " + linker + " ON " + linker + "." + type + "Id = " + type + ".Id " +
@@ -271,6 +277,9 @@ namespace GaMMBo.Test
                 name = sqlReader[0].ToString();
             }
             conn.Close();
+            // if the object has not been voted on the name printed out will be null 
+            // using another sql statement the objects name and description will be pulled from one of the four categories database
+            // if the name is not null the method calls itself to get another object id to test and display
             if (name == null)
             {
                 sqlCommand = new SqlCommand("Select " + type + ".Name , " + type + ".Description From " + type + " Where " + type + ".Id = @ID ", conn);
