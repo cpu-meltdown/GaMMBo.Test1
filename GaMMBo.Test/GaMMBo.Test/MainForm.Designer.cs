512c50864e0a38e3d969e58bf9abc82a99ca4265
yassine93 <bassima@cox.net>
2015-04-21 13:54:11 -0400

    added a image to main page

--- GaMMBo.Test/GaMMBo.Test/MainForm.Designer.cs
+++ GaMMBo.Test/GaMMBo.Test/MainForm.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
             this.btnLogin = new System.Windows.Forms.Button();
             this.btnNewUser = new System.Windows.Forms.Button();
             this.btnGuest = new System.Windows.Forms.Button();
@@ -67,7 +68,7 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(406, 68);
+            this.button1.Location = new System.Drawing.Point(499, 31);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 3;
@@ -77,7 +78,7 @@
             // 
             // btnExit
             // 
-            this.btnExit.Location = new System.Drawing.Point(449, 345);
+            this.btnExit.Location = new System.Drawing.Point(499, 356);
             this.btnExit.Name = "btnExit";
             this.btnExit.Size = new System.Drawing.Size(75, 23);
             this.btnExit.TabIndex = 4;
@@ -89,6 +90,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
             this.ClientSize = new System.Drawing.Size(615, 410);
             this.ControlBox = false;
             this.Controls.Add(this.btnExit);
